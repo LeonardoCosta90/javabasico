6527cc3e9bf50596b5b438db162d30915600a173
Matuk <mateusmatuk@fai-mg.br>
2020-03-02 19:54:47 -0300

    primeiro commit

---
+++ src/br/fai/projeto01/Main.java
@@ -0,0 +1,19 @@
+package br.fai.projeto01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Main app = new Main();
+		
+		app.start();
+
+	}
+
+	private void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
